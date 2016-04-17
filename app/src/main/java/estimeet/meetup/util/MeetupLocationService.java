3661de459f132fb6009bcc60b231764b4db0f70a
chris <chris.duan@gmail.com>
2016-04-17 16:10:45 +1200

    change gps update time

--- app/src/main/java/estimeet/meetup/util/MeetupLocationService.java
+++ app/src/main/java/estimeet/meetup/util/MeetupLocationService.java
@@ -32,11 +32,11 @@ public class MeetupLocationService implements GoogleApiClient.OnConnectionFailed
         LocationListener, SendGeoDataInteractor.SendGeoListener {
     private static final String TAG = MeetupLocationService.class.getSimpleName();
 //      // TODO: 10/04/16  live interval
-//    private static final int FASTEST_INTERVAL = 20000;
-//    private static final int UPDATE_INTERVAL = 40000;
+    private static final int FASTEST_INTERVAL = 20000;
+    private static final int UPDATE_INTERVAL = 40000;
     //// TODO: 10/04/16 debug interval
-    private static final int FASTEST_INTERVAL = 5000;
-    private static final int UPDATE_INTERVAL = 10000;
+//    private static final int FASTEST_INTERVAL = 5000;
+//    private static final int UPDATE_INTERVAL = 10000;
 
     private static GoogleApiClient googleApiClient;
     private static LocationRequest locationRequest;
